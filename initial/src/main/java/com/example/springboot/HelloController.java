5333169f1cf4ce4cf1030e710a948b950af033e1
Laila Hasben <laihasben@Lailas-MacBook-Pro.local>
2022-05-17 20:20:27 -0400

    first commit

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.springboot;
 
+import java.io.IOException;
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.File;  // Import the File class
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.LinkedList;
+
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    @GetMapping("/music")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
+    @GetMapping("/music/catalog")
+    public Music[] getCatalog() throws IOException {
+        //create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //read json file and convert to customer object
+        Music[] musicObjects= objectMapper.readValue(new File("songList.json"), Music[].class);
+
+
+
+        return musicObjects;
+    }
 }
+
+
+
+
+
+
---
+++ initial/src/main/java/com/example/springboot/Music.java
@@ -0,0 +1,24 @@
+package com.example.springboot;
+
+public class Music {
+
+    private String artist;
+    private String song;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+}
